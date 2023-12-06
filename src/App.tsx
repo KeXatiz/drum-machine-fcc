9865738864f4899381eb285846f7d36b7f976a12
DESKTOP-SHFSN5N\user <m.amirnaqiuddin@gmail.com>
2023-12-06 14:36:56 +0800

    pass the test witout styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import './App.css'
 import { AudioClip } from './types'
-import Drum from './Drum';
 
 //the : AudioClip[] is smtg like : string[] or : any[] where, expetect the string values, but using this it is more organized
 //we can also do const audioClip = [{key: "" , url: ""}] 
@@ -57,29 +56,44 @@ function App() {
   
   //By specifying clip: AudioClip, we provide type information
   const playSound = (clip:AudioClip) => {
-    const audio = new Audio(clip.url);
-    audio.play();
+    // const audio = new Audio(clip.url);
+    // audio.play();
+
+    (document.getElementById(clip.key) as HTMLAudioElement).play().catch(console.error);
+
+
+    //the ! operator is telling Typescript that the value will be non-null
+    document.getElementById("display")!.innerText = clip.description;
   }
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLDivElement>) => {
+    //change to upper case
+    const key = e.key.toUpperCase();
+    console.log(key);
+
+    const clip = audioClips.find((clip) => clip.key == key);
+
+    if(clip){
+      playSound(clip);
+    }
+    
+  }
 
   return (
     <>
-      <div className='container' id="drum-machine">
+      <div className='container' id="drum-machine" onKeyDown={handleKeyDown}>
           
-
           <div className="drum-bank">
             {audioClips.map((clip) => (
-              // <Drum audioClip={clip} key={clip.key} />
-              <button className='drum-pad' id={clip.key} key={clip.key} onClick={() => playSound(clip)}>
+
+              <button className='drum-pad' id={`drum-${clip.key}`} key={clip.key} onClick={() => playSound(clip)}>
                 <audio className="clip" src={clip.url} id={clip.key} /> 
                 {clip.key} 
               </button>
             ))}
           </div>
 
-          <div id="display">
-
-          </div>
+          <div id="display"></div>
       </div>
     </>
   )
